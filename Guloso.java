ba0f9eef6b31fce9e74518579d4a19492f037f2c
Lana-chan <erin.manal@gmail.com>
2014-12-22 04:01:26 -0200

    bugfix

--- Guloso.java
+++ Guloso.java
@@ -1,35 +1,35 @@
 public class Guloso {
   public static void guloso1(Mochila bag){
-    int[bag.itens.length] A;
+    int[] A = new int[bag.itens.length];
     int escolhido;
     for(int i = 0; i < bag.itens.length ; i++){
       for (int j = 0; j < bag.itens.length ; j++){
         if (bag.itens[i].valor/bag.itens[i].peso <= bag.itens[j].valor/bag.itens[j].peso){ //ordena o vetor em relação valor/peso
-          A[i]= j;
+          A[i] = j;
         } 
       }
     }
     for(int i = 0; i < bag.itens.length; i++){ //coloca os itens que foram ordenados dentro da mochila até encher
       bag.carga.add(A[i]);
       if (bag.capacidade - bag.pesoTotal() < 0){
-        bag.carga.remove (i);
+        bag.carga.remove(i);
         return;
       }
     }
   }
   
   public static void guloso2(Mochila bag){
-    int[bag.itens.length] A;
+    int[] A = new int[bag.itens.length];
     int escolhido;
     for(int i = 0; i < bag.itens.length ; i++){
       for (int j = 0; j < bag.itens.length ; j++){
         if (bag.itens[i].peso >= bag.itens[j].peso){ //ordena o vetor em relação valor/peso
-          A[i]= j;
+          A[i] = j;
         }
       }
     }
     for(int i = 0; i < bag.itens.length; i++){ //coloca os itens que foram ordenados dentro da mochila até encher
-      bag.carga.add (A[i]);
+      bag.carga.add(A[i]);
       if (bag.capacidade - bag.pesoTotal() < 0){
         bag.carga.remove(i);
         return;
