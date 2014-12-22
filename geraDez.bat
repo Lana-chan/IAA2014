844aa1b3b4555a79cb1d6c1e37a3da0d2abf9781
Lana-chan <erin.manal@gmail.com>
2014-12-22 05:33:23 -0200

    batches para testes rápidos

---
+++ geraDez.bat
@@ -0,0 +1,10 @@
+java geraTeste config0.txt
+java geraTeste config1.txt
+java geraTeste config2.txt
+java geraTeste config3.txt
+java geraTeste config4.txt
+java geraTeste config5.txt
+java geraTeste config6.txt
+java geraTeste config7.txt
+java geraTeste config8.txt
+java geraTeste config9.txt
---
+++ testaDez.bat
@@ -0,0 +1,10 @@
+java EP config0.txt
+java EP config1.txt
+java EP config2.txt
+java EP config3.txt
+java EP config4.txt
+java EP config5.txt
+java EP config6.txt
+java EP config7.txt
+java EP config8.txt
+java EP config9.txt
