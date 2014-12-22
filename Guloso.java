2b39b2c352fd64e1d4b18c334b3d713510348a0a
odinpower <odinpower@hotmail.com>
2014-12-22 01:29:04 -0200

    Guloso com segunda parte

--- Guloso.java
+++ Guloso.java
@@ -1,8 +1,8 @@
 public class Guloso{
-  public Mochila Guloso1(Mochila bag){
+  public void Guloso1(Mochila bag){
     Item aux = new Item();
     for(int i = 0; i < bag.itens.length - 1 ; i++){
-      if (bag.itens[i].valor/bag.itens.peso < bag.itens[i+1].valor/bag.itens[i+1].peso){
+      if (bag.itens[i].valor/bag.itens[i+1].peso < bag.itens[i+1].valor/bag.itens[i+1].peso){
         aux = bag.itens[i+1]
         bag.itens[i+1] = bag.itens[i]
         bag.itens[i]= aux;        
@@ -12,11 +12,25 @@ public class Guloso{
       bag.carga.add (i);
       if (bag.capacidade - bag.pesoTotal < 0){
         bag.carga.remove (i);
-        return bag;
+        return;
       }
     }
   }
-  public Item[] Guloso2(int peso, Item[] A){
-    
+  public void Guloso2(Mochila bag){
+    Item aux = new Item();
+    for(int i = 0; i < bag.itens.length - 1 ; i++){
+      if (bag.itens[i].peso > bag.itens[i+1].peso){
+        aux = bag.itens[i+1]
+        bag.itens[i+1] = bag.itens[i]
+        bag.itens[i]= aux;        
+      }
+    }
+    for(int i = 0; i < bag.itens.length; i++){
+      bag.carga.add (i);
+      if (bag.capacidade - bag.pesoTotal < 0){
+        bag.carga.remove (i);
+        return;
+      }
+    }
   }
 }
