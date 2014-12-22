4c51d3dab7c6d0a8e7648c0ee8d191d8a2a7048a
Lana-chan <erin.manal@gmail.com>
2014-12-22 02:28:14 -0200

    corrigindo merge

--- Guloso.java
+++ Guloso.java
@@ -2,29 +2,16 @@ public class Guloso {
   public static void guloso1(Mochila bag){
     Item aux = new Item();
     for(int i = 0; i < bag.itens.length - 1 ; i++){
-<<<<<<< HEAD
       if (bag.itens[i].valor/bag.itens[i+1].peso < bag.itens[i+1].valor/bag.itens[i+1].peso){ //ordena o vetor em relação valor/peso
         aux = bag.itens[i+1];
         bag.itens[i+1] = bag.itens[i];
-        bag.itens[i]= aux;        
+        bag.itens[i] = aux;
       }
     }
-    for(int i = 0; i < bag.itens.length; i++){ //coloca os itens que foram ordenados dentro da mochila até enxer
-      bag.carga.add (i);
-      if (bag.capacidade - bag.pesoTotal < 0){
-        bag.carga.remove (i);
-=======
-      if (bag.itens[i].valor/bag.itens[i+1].peso < bag.itens[i+1].valor/bag.itens[i+1].peso){
-        aux = bag.itens[i+1];
-        bag.itens[i+1] = bag.itens[i];
-        bag.itens[i] = aux;        
-      }
-    }
-    for(int i = 0; i < bag.itens.length; i++){
+    for(int i = 0; i < bag.itens.length; i++){ //coloca os itens que foram ordenados dentro da mochila até encher
       bag.carga.add(i);
       if (bag.capacidade - bag.pesoTotal() < 0){
-        bag.carga.remove(i);
->>>>>>> origin/master
+        bag.carga.remove (i);
         return;
       }
     }
@@ -33,20 +20,13 @@ public class Guloso {
   public static void guloso2(Mochila bag){
     Item aux = new Item();
     for(int i = 0; i < bag.itens.length - 1 ; i++){
-<<<<<<< HEAD
-      if (bag.itens[i].peso > bag.itens[i+1].peso){//ordena o vetor em relação do menor peso para o maior peso, tenta colocar a maior quantidade de itens possiveis na mochila
+      if (bag.itens[i].peso > bag.itens[i+1].peso){ //ordena o vetor em relação do menor peso para o maior peso, tenta colocar a maior quantidade de itens possiveis na mochila
         aux = bag.itens[i+1];
         bag.itens[i+1] = bag.itens[i];
-        bag.itens[i]= aux;        
-=======
-      if (bag.itens[i].peso > bag.itens[i+1].peso){
-        aux = bag.itens[i+1];
-        bag.itens[i+1] = bag.itens[i];
-        bag.itens[i] = aux;        
->>>>>>> origin/master
+        bag.itens[i] = aux;
       }
     }
-    for(int i = 0; i < bag.itens.length; i++){ //coloca os itens que foram ordenados dentro da mochila até enxer
+    for(int i = 0; i < bag.itens.length; i++){ //coloca os itens que foram ordenados dentro da mochila até encher
       bag.carga.add (i);
       if (bag.capacidade - bag.pesoTotal() < 0){
         bag.carga.remove(i);
