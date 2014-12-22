7d534a0ab02b71bd0e8524580a387efb91097973
Lana-chan <erin.manal@gmail.com>
2014-12-22 02:14:22 -0200

    bugfixes

--- Guloso.java
+++ Guloso.java
@@ -1,34 +1,35 @@
-public class Guloso{
-  public void Guloso1(Mochila bag){
+public class Guloso {
+  public static void guloso1(Mochila bag){
     Item aux = new Item();
     for(int i = 0; i < bag.itens.length - 1 ; i++){
       if (bag.itens[i].valor/bag.itens[i+1].peso < bag.itens[i+1].valor/bag.itens[i+1].peso){
-        aux = bag.itens[i+1]
-        bag.itens[i+1] = bag.itens[i]
-        bag.itens[i]= aux;        
+        aux = bag.itens[i+1];
+        bag.itens[i+1] = bag.itens[i];
+        bag.itens[i] = aux;        
       }
     }
     for(int i = 0; i < bag.itens.length; i++){
-      bag.carga.add (i);
-      if (bag.capacidade - bag.pesoTotal < 0){
-        bag.carga.remove (i);
+      bag.carga.add(i);
+      if (bag.capacidade - bag.pesoTotal() < 0){
+        bag.carga.remove(i);
         return;
       }
     }
   }
-  public void Guloso2(Mochila bag){
+  
+  public static void guloso2(Mochila bag){
     Item aux = new Item();
     for(int i = 0; i < bag.itens.length - 1 ; i++){
       if (bag.itens[i].peso > bag.itens[i+1].peso){
-        aux = bag.itens[i+1]
-        bag.itens[i+1] = bag.itens[i]
-        bag.itens[i]= aux;        
+        aux = bag.itens[i+1];
+        bag.itens[i+1] = bag.itens[i];
+        bag.itens[i] = aux;        
       }
     }
     for(int i = 0; i < bag.itens.length; i++){
       bag.carga.add (i);
-      if (bag.capacidade - bag.pesoTotal < 0){
-        bag.carga.remove (i);
+      if (bag.capacidade - bag.pesoTotal() < 0){
+        bag.carga.remove(i);
         return;
       }
     }
