541fded1811c1657dd24f91e35d0aa21e3695c9d
odinpower <odinpower@hotmail.com>
2014-12-22 00:57:11 -0200

    Guloso certo omg.

---
+++ Guloso.java
@@ -0,0 +1,22 @@
+public class Guloso{
+  public Mochila Guloso1(Mochila bag){
+    Item aux = new Item();
+    for(int i = 0; i < bag.itens.length - 1 ; i++){
+      if (bag.itens[i].valor/bag.itens.peso < bag.itens[i+1].valor/bag.itens[i+1].peso){
+        aux = bag.itens[i+1]
+        bag.itens[i+1] = bag.itens[i]
+        bag.itens[i]= aux;        
+      }
+    }
+    for(int i = 0; i < bag.itens.length; i++){
+      bag.carga.add (i);
+      if (bag.capacidade - bag.pesoTotal < 0){
+        bag.carga.remove (i);
+        return bag;
+      }
+    }
+  }
+  public Item[] Guloso2(int peso, Item[] A){
+    
+  }
+}
